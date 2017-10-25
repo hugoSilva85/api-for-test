d5cdd01ff27c0ddd66b9b2d05e2e6a2e80a4b5a9
hugo.silva <hugo.silva@moipadmins-MacBook-Pro.local>
2017-10-25 13:42:01 -0200

    Added content_type and parameters to search by get

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'json'
 require './utils/ferrari'
+require './utils/volks'
 require './utils/proposta'
 require './utils/feedback'
 require 'pry-byebug'
@@ -8,17 +9,23 @@ require 'pry-byebug'
 set :bind, '0.0.0.0'
 set :environment, :production
 
-get '/' do
-  response = Ferrari.composicao
+get '/carros' do
+  content_type :json
+  # matches "GET /carros?marca=ferrari&modelo=maranello"
+  marca = params[:marca]
+
+  response = marca.constantize.composicao
   json = JSON.parse(response)
   JSON.pretty_generate(json.to_h)
 end
 
 post '/compra' do
+  content_type :json
+
   request_body = JSON.parse(request.body.read)
   objeto_serealizado = Proposta.from_json(request_body)
 
   response = Feedback.resposta_fianceira(objeto_serealizado)
   json = JSON.parse(response)
-  JSON.pretty_generate(json.to_h)
+  JSON.pretty_generate(response.to_h)
 end
